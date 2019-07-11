8884ae1b3bfdacce1f17dc01e8ecf430c77cca74
infitake <rvndryadv@gmail.com>
2019-07-11 18:21:34 +0530

    remove google outh

--- client/src/components/community/story/welcome.js
+++ client/src/components/community/story/welcome.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 // import { Redirect } from 'react-router'
-import GoogleLogin from 'react-google-login';
-import { GoogleLogout } from 'react-google-login';
 
 class Welcome extends Component {
     componentWillMount(){
@@ -29,19 +27,7 @@ class Welcome extends Component {
                 <h1>Hey, Welcome</h1>
                 <h3>Welcome to Storybook by Edumonk</h3>
                 <p>Post stories from the best and worst of your life and choose for them to be read by the world or completley private as your own personal diary</p>
-                <GoogleLogin
-                    clientId="952802453575-60mlt8b42glrj82tk6nvu5fl2qviudeg.apps.googleusercontent.com"
-                    buttonText="Login"
-                    onSuccess={this.responseGoogle}
-                    onFailure={this.responseGoogle}
-                    cookiePolicy={'single_host_origin'}
-                />
-                <GoogleLogout
-                    clientId="952802453575-60mlt8b42glrj82tk6nvu5fl2qviudeg.apps.googleusercontent.com"
-                    buttonText="Logout"
-                    onLogoutSuccess={this.logout}
-                >
-                </GoogleLogout>
+                
             </div>
         )
     }
